28ab224c77c191be930bd53165cfea28700845d1
kiannaquines <kjgnaquines@gmail.com>
2024-12-21 01:44:44 +0800

    added new code for practice

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/login', function(){
-    return 'Login Screen';
-});
+Route::get('/',[HomePageController::class,'index'])->name('index');
+Route::get('/login',[LoginController::class,'login'])->name('login');
+Route::get('/dashboard',[DashboardController::class,'dashboard'])->name('dashboard');
